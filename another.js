19da9a3628125fd469a840edc1a669f9bc179955
blakelower <blakelower1999@gmail.com>
2020-07-22 22:07:50 -0700

    got the buttons and images to shuffle

---
+++ another.js
@@ -0,0 +1,26 @@
+jQuery(function($) {
+    var arr = new Array('./images/download.jpg', './images/download-1.jpg', './images/download-2.jpg')
+    $.each($.shuffle(arr), function (_, src) {
+        $('<img />', {
+            src: src
+        }).appendTo('#array')
+    })
+    $('button.array').click(function () {
+        $('#array').shuffle()
+    });
+});
+
+(function ($) {
+    $.fn.shuffle = function () {
+        var _self = this,
+        children = $.shuffle(this.children().get());
+        $.each(children, function () {
+            _self.append(this)
+        })
+        return this;
+    };
+    $.shuffle = function (arr) {
+        for (var j,x,i = arr.length; i; j=parseInt(Math.random() * i), x = arr[--i], arr[i]=arr[j], arr[j] = x);
+        return arr
+    };
+})(jQuery)

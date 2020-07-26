16d8d843acf5d22ad9f70c83ab5381c04f69420a
blakelower <blakelower1999@gmail.com>
2020-07-26 15:10:29 -0700

    added pants

--- another.js
+++ another.js
@@ -1,38 +1,38 @@
-// var p1 = new Image();
-// var p2 = new Image();
-// var p3 = new Image();
-// var p4 = new Image();
-// var p5 = new Image();
-// var p6 = new Image();
-// var p7 = new Image();
-// var p8 = new Image();
+var p1 = new Image();
+var p2 = new Image();
+var p3 = new Image();
+var p4 = new Image();
+var p5 = new Image();
+var p6 = new Image();
+var p7 = new Image();
+var p8 = new Image();
 
-// p1.src="./images/download.jpg";
-// p2.src="./images/download-1.jpg";
-// p3.src="./images/download-2.jpg";
-// p4.src="./images/images.jpg";
-// p5.src="./images/images-1.jpg";
-// p6.src="./images/images-2.jpg";
-// p7.src="./images/images-3.jpg";
+p1.src="./images/download.jpg";
+p2.src="./images/download-4.jpg";
+p3.src="./images/img4.jpg";
+p4.src="./images/download-5.jpg";
+p5.src="./images/download-4.jpg";
+p6.src="./images/img3.jpg";
+p7.src="./images/img1.jpg";
 
-// var imgArray = new Array(p1, p2, p3, p4, p5, p6, p7);
+var imgArray2 = new Array(p1, p2, p3, p4, p5, p6, p7);
 
-// var counter = 0;
-// var end = imgArray.length - 1;
+var counter = 0;
+var end = imgArray2.length - 1;
 
 
-// function scroll_back() {
-//     if (counter == 0){
-//     } else {
-//         counter--;
-//     }
-//     document.pic1.src = imgArray[counter].src;
-// }
+function scroll_back() {
+    if (counter == 0){
+    } else {
+        counter--;
+    }
+    document.pic2.src = imgArray2[counter].src;
+}
 
-// function scroll_for(){
-//     if (counter == end){
-//     } else {
-//         counter++;
-//     }
-//     document.pic1.src = imgArray[counter].src;
-// }
+function scroll_for(){
+    if (counter == end){
+    } else {
+        counter++;
+    }
+    document.pic2.src = imgArray2[counter].src;
+}
--- index.js
+++ index.js
@@ -5,16 +5,14 @@ var p4 = new Image();
 var p5 = new Image();
 var p6 = new Image();
 var p7 = new Image();
-var p8 = new Image();
 
 p1.src="./images/download.jpg";
 p2.src="./images/download-1.jpg";
 p3.src="./images/download-2.jpg";
-p4.src="./images/images.jpg";
-p5.src="./images/images-1.jpg";
+p4.src="./images/images-3.jpg";
+p5.src="./images/images-3.jpg";
 p6.src="./images/images-2.jpg";
 p7.src="./images/images-3.jpg";
-// p8.src="./images/images-5.jpg";
 
 var imgArray = new Array(p1, p2, p3, p4, p5, p6, p7);
 
